5c1f97dd1d3bea579c2a5c3cba0e62f51a30844c
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2024-11-23 09:23:58 +0530

    add tsconfig.json and connect the database

---
+++ src/configs/constant.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGODB_URI: string = process.env.MONGODB_URI!;
+
+export { MONGODB_URI };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-const x: string = "Hellow from brainly app";
-console.log(x);
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./configs/database";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Server is in running mode...</h1>");
+});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Mongodb connection failed !!! ${err}`);
+  });
