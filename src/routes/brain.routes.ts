603c623f0d568f11559b0cb34785206c45a44187
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2024-11-25 17:43:15 +0530

    fixe typo

--- src/routes/brain.routes.ts
+++ src/routes/brain.routes.ts
@@ -8,6 +8,6 @@ import {
 const router = Router();
 
 router.route("/share").post(isAuthenticated as any, createSharedLink); // API 1: http://localhost:3000/api/v1/brain/share
-router.route("/:sharedHash").get(getSharedLink); // API 2: http://localhost:3000/api/v1/brain/get
+router.route("/:sharedHash").get(getSharedLink); // API 2: http://localhost:3000/api/v1/brain/:sharedHash
 
 export default router;
